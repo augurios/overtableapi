602d505118ba606922dd9ce8270fe92e302b9ddc
Augusto Valerio <auguvalerio@gmail.com>
2018-08-30 23:28:47 -0600

    db update

--- server/config/database.js
+++ server/config/database.js
@@ -14,7 +14,7 @@ var mongoose = require('mongoose');
 //const MGDCMT = 'meanapp';
 
 //Dev Database Details 
-const MGHOST = 'overtableapp.disruptive.pro';
+const MGHOST = 'ec2-52-36-100-46.us-west-2.compute.amazonaws.com';
 const MGUSER = 'dbadminapp';
 const MGPWWD = 'secoelpinto';
 const MGPORT = '27017';
